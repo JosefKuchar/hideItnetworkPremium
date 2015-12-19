c7a1044c953ff1c379fb4766c93ab9171e0d3e1d
Josef Kuchař <josef.kuchar267@gmail.com>
2015-12-19 11:30:35 +0100

    Update hideItnetworkPremium.user.js
    Added PR article remover

--- hideItnetworkPremium.user.js
+++ hideItnetworkPremium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide Itnetwork Premium
 // @namespace    none
-// @version      1.0.0
+// @version      2.0.0
 // @description  Hide premium articles!
 // @author       Josef Kuchař
 // @match        http://www.itnetwork.cz/
@@ -12,3 +12,8 @@ var element = $(".wall-box:contains('premium článek')");
 element.next().next().remove();
 element.next().remove();
 element.remove();
+
+var element2 = $(".wall-box:contains('PR článek')");
+element2.next().next().remove();
+element2.next().remove();
+element2.remove();
